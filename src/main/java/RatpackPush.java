ecd16801658d10edcf2e7320dbed7655eb7a3eef
Samuel <samuel.olaechea@gmail.com>
2021-12-07 18:27:08 -0400

    Update demo to not reload the full table when an document is updated in the back-end.

--- src/main/java/RatpackPush.java
+++ src/main/java/RatpackPush.java
@@ -1,7 +1,7 @@
 import ratpack.sse.ServerSentEvents;
 import ratpack.server.RatpackServer;
 import ratpack.server.BaseDir;
-import static ratpack.jackson.Jackson.toJson;
+import ratpack.jackson.Jackson;
 import static ratpack.jackson.Jackson.chunkedJsonList;
 
 import com.mongodb.client.model.Aggregates;
@@ -13,7 +13,10 @@ import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoCollection;
 import com.mongodb.reactivestreams.client.MongoDatabase;
 
-import static java.util.Arrays.asList;
+import java.util.Arrays;
+import java.util.List;
+import org.bson.BsonString;
+import org.bson.BsonDocument;
 import org.bson.Document;
 
 public class RatpackPush {
@@ -21,24 +24,42 @@ public class RatpackPush {
   public static void main(String[] args) throws Exception {
 
     MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
-    MongoDatabase database = mongoClient.getDatabase("test");
-    MongoCollection<Document> collection = database.getCollection("restaurants");
 
-    ChangeStreamPublisher<Document> changes = collection.watch(asList(
-      Aggregates.match(in("operationType", asList("insert", "update")))));
+
+    MongoDatabase database = mongoClient.getDatabase("test");
+    MongoCollection<Document> collection = database.getCollection("grades");
+
+    List operationTypes = Arrays.asList("insert", "update", "delete", "replace");
+    List pipeline = Arrays.asList(Aggregates.match(in("operationType", operationTypes)));
+
+    ChangeStreamPublisher<Document> changes;
+    String rToken = System.getProperty("rToken");
+
+    if(rToken == null){
+      changes = collection.watch(pipeline);
+    }else{
+      BsonDocument resumeToken = new BsonDocument("_data", new BsonString(rToken));
+      changes = collection.watch(pipeline).resumeAfter(resumeToken);
+    }
 
     RatpackServer.start(server -> server
       .serverConfig(c -> c.baseDir(BaseDir.find()))
       .handlers(chain -> chain
         .files(f -> f.dir("public").indexFiles("index.html"))
         .get("jsonData", ctx -> {
-          ctx.render(chunkedJsonList(ctx, collection.find()));
+          ctx.render(chunkedJsonList(ctx, collection.find(lte("classId", 20d))));
         })
         .get("dataGrid", ctx -> {
             ServerSentEvents events = ServerSentEvents.serverSentEvents(changes, dataGridEvent -> {
                 dataGridEvent.event("message");
                 dataGridEvent.id(Long.toString(System.currentTimeMillis()));
-                dataGridEvent.data(toJson(ctx));
+
+                dataGridEvent.data(eventData -> {
+                  eventData.getDocumentKey().put("hexaId",
+                          new BsonString(eventData.getDocumentKey().getObjectId("_id").getValue().toString()));
+
+                  return Jackson.getObjectWriter(ctx).writeValueAsString(eventData);
+                });
             });
             ctx.render(events);
         })
